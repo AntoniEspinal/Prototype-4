d997f8a6a88570a764561f3900d32cc10f168827
AntoniEspinal <97680717+AntoniEspinal@users.noreply.github.com>
2023-02-06 12:30:17 -0500

    ook

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody enemyRb;
+    private GameObject player;
+    public  GameObject enemyPrefab;
+    private float spawnRange= 9;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRb = GetComponent<Rigidbody>();
+        player = GameObject.Find("Player");
+        Instantiate(enemyPrefab, GenerateSpawnPostion(), enemyPrefab.transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+        Vector3 lookDirection = (player.transform.position - transform.position).normalized;
+        enemyRb.AddForce(lookDirection.normalized * speed);
+    }
+
+    private Vector3 GenerateSpawnPostion()
+    {
+        float spawnPosX = Random.Range(-spawnRange, spawnRange);
+        float spawnPosZ = Random.Range(-spawnRange, spawnRange);
+        Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
+        return randomPos;
+    }
+}
