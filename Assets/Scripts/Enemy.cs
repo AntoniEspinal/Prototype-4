892a31c63c34b67845bd7df1ec0a7decc949d3dd
AntoniEspinal <97680717+AntoniEspinal@users.noreply.github.com>
2023-02-07 12:09:54 -0500

    ok

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,15 +7,12 @@ public class Enemy : MonoBehaviour
     public float speed;
     private Rigidbody enemyRb;
     private GameObject player;
-    public  GameObject enemyPrefab;
-    private float spawnRange= 9;
 
     // Start is called before the first frame update
     void Start()
     {
         enemyRb = GetComponent<Rigidbody>();
         player = GameObject.Find("Player");
-        Instantiate(enemyPrefab, GenerateSpawnPostion(), enemyPrefab.transform.rotation);
     }
 
     // Update is called once per frame
@@ -23,13 +20,5 @@ public class Enemy : MonoBehaviour
     { 
         Vector3 lookDirection = (player.transform.position - transform.position).normalized;
         enemyRb.AddForce(lookDirection.normalized * speed);
-    }
-
-    private Vector3 GenerateSpawnPostion()
-    {
-        float spawnPosX = Random.Range(-spawnRange, spawnRange);
-        float spawnPosZ = Random.Range(-spawnRange, spawnRange);
-        Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
-        return randomPos;
     }
 }
